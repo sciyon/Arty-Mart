fa14440ab9f71cc97e1890fcaa33efef85e9d042
Erwin Antepuesto <matcha.ccy@gmail.com>
2023-12-16 18:53:13 +0800

    Erwin | FE and BE are now connected together

---
+++ models/users.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+interface User{
+  id?: String,
+  fname: String, 
+  lname: String,
+  email: String,
+  password: String,
+  roles: String[],
+  status: String,
+  createdOn: Date
+}
+
+const UserSchema = new Schema<User>({
+  id: String,
+  fname: { type: String, required: true},
+  lname: { type: String, required: true},
+  email: { type: String, required: true},
+  password: { type: String, required: true},
+  roles: { type: [String], required: true },
+  status: { type: String, required: true },
+  createdOn: { type: Date, default: Date.now },
+})
+
+const User = model<User>('user', UserSchema);
+
+export default User;
