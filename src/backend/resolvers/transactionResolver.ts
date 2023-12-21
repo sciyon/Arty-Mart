a5be29c0acabad021aaed549907e702e077c8b4f
Erwin Antepuesto <matcha.ccy@gmail.com>
2023-12-21 22:43:26 +0800

    scion | added error catching to the transaction queries

--- src/backend/resolvers/transactionResolver.ts
+++ src/backend/resolvers/transactionResolver.ts
@@ -1,23 +1,70 @@
 import { GraphQLError } from 'graphql';
 
 import Transaction from '../models/transaction.js';
+import User from '../models/users.js';
+import Artwork from '../models/artworks.js';
 
 const transactionResolver = {
   Query: {
     async transactionGetFromUser(_, { buyerID }){
-      return Transaction.find({ 'buyerID': buyerID });
+      const user = await User.findById(buyerID);
+      if(!user){
+        throw new GraphQLError("User does not exist.", {
+          extensions: { code: 'USER_DOESNT_ARTIST'}
+        })
+      }
+
+      try {
+        return Transaction.find({ 'buyerID': buyerID });
+      } catch (error) {
+        throw new GraphQLError("Failed to get t ransactions from buyer.", {
+          extensions: { code: 'BUYER_TRANSACTION_FETCH_FAIL'}
+        })
+      }
     },
  
     async transactionGetFromArtist(_, { artistID }){
-      return Transaction.find({ 'artistID': artistID });
+      const user = await User.findById(artistID);
+      if(!user){
+        throw new GraphQLError("Artist does not exist.", {
+          extensions: { code: 'ARTIST_DOESNT_ARTIST'}
+        })
+      }
+
+      try{
+        return Transaction.find({ 'artistID': artistID });
+      }catch(error){
+        throw new GraphQLError("Failed to get transactions from artist.", {
+          extensions: { code: 'ARTIST_TRANSACTION_FETCH_FAIL'}
+        })
+      }
     },
  
     async transactionGetFromArtwork(_, { artworkID }){
-      return Transaction.find({ 'artworkID': artworkID });
+      const artwork = Artwork.findById(artworkID)
+      if(!artwork){
+        throw new GraphQLError("Artwork does not exist.", {
+          extensions: { code: 'ARTWORK_DOESNT_ARTIST'}
+        })
+      }
+
+      try {
+        return Transaction.find({ 'artworkID': artworkID });
+      } catch (error) {
+        throw new GraphQLError("Failed to get transactions from artwork.", {
+          extensions: { code: 'ARTWORK_TRANSACTION_FETCH_FAIL'}
+        })
+      }
     },
 
     async transactionGetFromID(_, { ID }){
-      return Transaction.findById(ID);
+      try {
+        return Transaction.findById(ID);
+      } catch (error) {
+        throw new GraphQLError("Failed to get transaction from ID.", {
+          extensions: { code: 'ID_TRANSACTION_FETCH_FAIL'}
+        })
+      }
     }
   },
  
